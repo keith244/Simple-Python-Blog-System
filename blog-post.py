419aa555147a1d636f20d26162ee1c8b54cc0d33
Keith <keithkipngeno24@gmail.com>
2025-05-21 18:25:02 +0300

    Initial commit

---
+++ blog-post.py
@@ -0,0 +1,101 @@
+class User:
+    def __init__(self,username):
+        self.username = username
+
+class BlogPost:
+    blog_list = []
+    def __init__(self,user,title='',body=''):
+        self.title = title
+        self.body = body
+        self.user = user
+    
+
+    def user_action(self,):
+        while True:
+            user_choice = input('\nWhat would you like to do?\n1. Create Post\n2. Read posts\n3. Delete post\n4. Exit\nYour choice: ')
+            
+            if user_choice == '1':
+                self.create_post()
+            elif user_choice == '2':
+                self.show_posts()
+            elif user_choice == '3':
+                self.delete_post()
+            elif user_choice == '4' or user_choice.lower() in ['exit', 'quit']:
+                print('Exiting the blog program. Goodbye!')
+                break
+            else:
+                print('Invalid choice. Try again!')
+
+    
+    def create_post(self,):
+        self.title = str(input('Enter blog title: ')).strip()
+        self.body = str(input('Body : ')).strip()
+
+        if self.title and self.body:
+            BlogPost.blog_list.append(self)
+            print(f"Post '{self.title}' created successfully!")
+
+        else:
+            print('Error: Both title and body are required.')
+
+
+    def show_posts(self):
+        if not BlogPost.blog_list:
+            print('No posts to show.')
+            return 
+        print()
+        print("\n==== ALL POSTS ====")
+        for i,blog in enumerate(BlogPost.blog_list, start=1):
+            print(f'Post {i}:\n{blog.user.username}: \n{blog}')
+        print()
+        print("n==== END ====")
+        print()
+
+    
+    def delete_post(self,):
+        if not BlogPost.blog_list:
+            print('No posts to delete.')
+            return 
+        
+        print('\nAvailable Posts:')
+        for i, blog in enumerate(BlogPost.blog_list,1):
+            print(f'{i}. {blog.title} (by {blog.user.username})')
+        try:
+            index = int(input('\nEnter post number to delete (0 to cancel): ')) -1
+
+            if index == -1:
+                print('Deletion canceled.')
+                return
+            if 0 <=index < len (BlogPost.blog_list):
+                removed = BlogPost.blog_list.pop(index)
+                print(f'Deleted Post: {removed.title}')
+            else:
+                print('Invalid post number')
+        except ValueError:
+            print('Please enter a valid number.')
+
+             
+    def __str__(self):
+        return f"--- {self.title.upper()} ---\nAuthor: {self.user.username}\n{self.body}\n"
+
+def main():
+    print('Welcome to My Blog App!')
+    username = input('Enter your username: ')
+    current_user = User(username)
+
+    blog = BlogPost(current_user)
+
+    blog.user_action()
+
+if __name__ == "__main__":
+    main()
+
+
+
+# post = BlogPost()
+# post.create_post()
+
+# another = BlogPost()
+# another.create_post()
+
+# post.show_posts()
